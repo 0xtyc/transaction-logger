bb5587f3407188c07eea35577d88c165a6fd0d17
0xtyc <tyc.tingyuchen@gmail.com>
2024-06-23 17:30:14 +0200

    Add sendMultiFunds and deploy to sepolia

--- contracts/TransactionLogger.sol
+++ contracts/TransactionLogger.sol
@@ -2,8 +2,10 @@
 
 pragma solidity ^0.8.24;
 
-error TransactionLogger__InsufficientFunds();
-error TransactionLogger__sendFundsFailed();
+error TransactionLogger__MininumTransferAmountNotMet();
+error TransactionLogger__InsufficientOrExcessFunds();
+error TransactionLogger__MismatchedReceiversAndAmounts();
+error TransactionLogger__TransactionFailed();
 
 contract TransactionLogger {
   // Event to log the sent transaction
@@ -14,15 +16,46 @@ contract TransactionLogger {
     uint256 timestamp
   );
 
+  uint256 public constant MINIMUM_AMOUNT = 0.0005 ether;
+
+  function _makeTransaction(address sender, address receiver, uint256 amount) internal {
+    (bool sent, ) = receiver.call{value: amount}("");
+    if (!sent) {
+      revert TransactionLogger__TransactionFailed();
+    }
+    emit TransactionSent(sender, receiver, amount, block.timestamp);
+  }
+
   // Function to send funds and record the transaction via an event
   function sendFunds(address payable receiver) public payable {
-    if (msg.value <= 0) {
-      revert TransactionLogger__InsufficientFunds();
+    if (msg.value <= MINIMUM_AMOUNT) {
+      revert TransactionLogger__MininumTransferAmountNotMet();
     }
-    (bool sent, ) = receiver.call{value: msg.value}("");
-    if (!sent) {
-      revert TransactionLogger__sendFundsFailed();
+    _makeTransaction(msg.sender, receiver, msg.value);
+  }
+
+  function sendMultiFunds(
+    address payable[] memory receivers,
+    uint256[] memory amounts
+  ) public payable {
+    if (receivers.length != amounts.length) {
+      revert TransactionLogger__MismatchedReceiversAndAmounts();
     }
-    emit TransactionSent(msg.sender, receiver, msg.value, block.timestamp);
+
+    uint256 totalAmount = 0;
+    for (uint256 i = 0; i < amounts.length; i++) {
+      if (amounts[i] <= MINIMUM_AMOUNT) {
+        revert TransactionLogger__MininumTransferAmountNotMet();
+      }
+      totalAmount += amounts[i];
+    }
+
+    if (msg.value != totalAmount) {
+      revert TransactionLogger__InsufficientOrExcessFunds();
+    }
+
+    for (uint256 i = 0; i < receivers.length; i++) {
+      _makeTransaction(msg.sender, receivers[i], amounts[i]);
+    }
   }
 }
--- test/TransactionLogger.ts
+++ test/TransactionLogger.ts
@@ -1,80 +1,187 @@
 import { expect } from "chai";
 import { ethers, ignition } from "hardhat";
 import TransactionLoggerModule from "../ignition/modules/TransactionLogger";
+import { Contract } from "ethers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("TransactionLogger", function () {
+  let txLogger: Contract;
+  let signers: HardhatEthersSigner[];
+  let receiver: string;
+  let receiver1: string;
+
+  beforeEach(async function () {
+    const deployment = await ignition.deploy(TransactionLoggerModule);
+    txLogger = deployment.txLogger;
+
+    // Get the list of local testing accounts and use them as receivers
+    signers = await ethers.getSigners();
+    receiver = signers[1].address;
+    receiver1 = signers[2].address;
+  });
+
   describe("sendFunds", function () {
-    describe("sendFunds", function () {
-      it("Should revert if send 0 eth", async function () {
-        const { txLogger } = await ignition.deploy(TransactionLoggerModule);
-        const amountToSend = ethers.parseEther("0");
-        // Get the list of local testing accounts
-        const signers = await ethers.getSigners();
-
-        // Use the second account as the receiver
-        const receiver = signers[1].address;
-        await expect(
-          txLogger.sendFunds(receiver, { value: amountToSend }),
-        ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__InsufficientFunds");
-      });
-
-      it("Should revert when _call fails", async function () {
-        const { txLogger } = await ignition.deploy(TransactionLoggerModule);
-        const amountToSend = ethers.parseEther("1");
-
-        // Deploy the RevertingContract
-        const RevertingContract = await ethers.getContractFactory("RevertingContract");
-        const revertingContract = await RevertingContract.deploy();
-
-        // Expect the transaction to be reverted
-        await expect(
-          txLogger.sendFunds(revertingContract.getAddress(), { value: amountToSend }),
-        ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__sendFundsFailed");
-        // contract balance should be 0
-        const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
-        expect(contractBalance).to.equal(0);
-      });
-
-      it("Should send funds to the receiver", async function () {
-        const { txLogger } = await ignition.deploy(TransactionLoggerModule);
-        const amountToSend = ethers.parseEther("1");
-
-        const signers = await ethers.getSigners();
-
-        // Use the second account as the receiver
-        const receiver = signers[1].address;
-        const receiverBalanceBefore = await ethers.provider.getBalance(receiver);
-        await txLogger.sendFunds(receiver, { value: amountToSend });
-        const receiverBalanceAfter = await ethers.provider.getBalance(receiver);
-        const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
-        // Check the receiver balance increased by the amount sent and the contract balance is 0
-        expect(receiverBalanceAfter).to.equal(receiverBalanceBefore + amountToSend);
-        expect(contractBalance).to.equal(0);
-      });
+    it("Should revert if send less than mininum eth", async function () {
+      const amountToSend = ethers.parseEther("0.0001");
+      await expect(
+        txLogger.sendFunds(receiver, { value: amountToSend }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__MininumTransferAmountNotMet");
     });
 
-    describe("Events", function () {
-      it("Should emit an event on sendFunds success", async function () {
-        const { txLogger } = await ignition.deploy(TransactionLoggerModule);
-        const amountToSend = ethers.parseEther("1");
+    it("Should revert when _call fails", async function () {
+      const amountToSend = ethers.parseEther("1");
 
-        const signers = await ethers.getSigners();
+      // Deploy the RevertingContract
+      const RevertingContract = await ethers.getContractFactory("RevertingContract");
+      const revertingContract = await RevertingContract.deploy();
 
-        // Use the second account as the receiver
-        const receiver = signers[1].address;
+      // Expect the transaction to be reverted
+      await expect(
+        txLogger.sendFunds(revertingContract.getAddress(), { value: amountToSend }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__TransactionFailed");
+      // contract balance should be 0
+      const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
+      expect(contractBalance).to.equal(0);
+    });
 
-        // Send funds and get the transaction receipt
-        const tx = await txLogger.sendFunds(receiver, { value: amountToSend });
-        const receipt = await tx.wait();
+    it("Should send funds to the receiver", async function () {
+      const amountToSend = ethers.parseEther("1");
 
-        // Get the block and its timestamp
-        const block = await ethers.provider.getBlock(receipt.blockNumber);
-        const timestamp = block?.timestamp;
+      const receiverBalanceBefore = await ethers.provider.getBalance(receiver);
+      await txLogger.sendFunds(receiver, { value: amountToSend });
+      const receiverBalanceAfter = await ethers.provider.getBalance(receiver);
+      const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
+      // Check the receiver balance increased by the amount sent and the contract balance is 0
+      expect(receiverBalanceAfter).to.equal(receiverBalanceBefore + amountToSend);
+      expect(contractBalance).to.equal(0);
+    });
+  });
 
-        await expect(tx)
-          .to.emit(txLogger, "TransactionSent")
-          .withArgs(signers[0], receiver, amountToSend, timestamp);
+  describe("sendMultipleFunds", function () {
+    it("Should revert if send less than mininum eth", async function () {
+      const amountsToSend = [ethers.parseEther("0.0001"), ethers.parseEther("0.5")];
+      const totalAmount = ethers.parseEther("0.5001");
+
+      await expect(
+        txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, { value: totalAmount }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__MininumTransferAmountNotMet");
+    });
+
+    it("Should revert when number of receivers is larger than number of amount", async function () {
+      const amountsToSend = [ethers.parseEther("0.1")];
+      const totalAmount = ethers.parseEther("0.1");
+      await expect(
+        txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, { value: totalAmount }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__MismatchedReceiversAndAmounts");
+    });
+
+    it("Should revert when number of receivers is fewer than number of amount", async function () {
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.1")];
+      const totalAmount = ethers.parseEther("0.2");
+      await expect(
+        txLogger.sendMultiFunds([receiver], amountsToSend, { value: totalAmount }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__MismatchedReceiversAndAmounts");
+    });
+
+    it("Should revert when sum of amounts is less than sent amount", async function () {
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.3")];
+
+      await expect(
+        txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, {
+          value: ethers.parseEther("0.3"),
+        }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__InsufficientOrExcessFunds");
+    });
+
+    it("Should revert when sum of amounts is more than sent amount", async function () {
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.3")];
+
+      await expect(
+        txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, {
+          value: ethers.parseEther("0.5"),
+        }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__InsufficientOrExcessFunds");
+    });
+
+    it("Should revert all transactions when one of the transaction fails", async function () {
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.5")];
+      const totalAmount = ethers.parseEther("0.6");
+      const receiverBalanceBefore = await ethers.provider.getBalance(receiver);
+      const receiver1BalanceBefore = await ethers.provider.getBalance(receiver1);
+
+      // Deploy the RevertingContract
+      const RevertingContract = await ethers.getContractFactory("RevertingContract");
+      const revertingContract = await RevertingContract.deploy();
+
+      // Expect the transaction to be reverted
+      await expect(
+        txLogger.sendMultiFunds([receiver, revertingContract.getAddress()], amountsToSend, { value: totalAmount }),
+      ).to.be.revertedWithCustomError(txLogger, "TransactionLogger__TransactionFailed");
+      
+      const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
+      const receiverBalanceAfter = await ethers.provider.getBalance(receiver);
+      // the receiver balance should be the same as before and the contract balance should be 0
+      expect(receiverBalanceAfter).to.equal(receiverBalanceBefore);
+      expect(contractBalance).to.equal(0);
+    });
+
+    it("Should send funds to all receivers", async function () {
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.5")];
+      const totalAmount = ethers.parseEther("0.6");
+      const receiverBalanceBefore = await ethers.provider.getBalance(receiver);
+      const receiver1BalanceBefore = await ethers.provider.getBalance(receiver1);
+
+      await txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, { value: totalAmount });
+
+      const receiverBalanceAfter = await ethers.provider.getBalance(receiver);
+      const receiver1BalanceAfter = await ethers.provider.getBalance(receiver1);
+      const contractBalance = await ethers.provider.getBalance(txLogger.getAddress());
+      // Check the receiver balance increased by the amount sent and the contract balance is 0
+      expect(receiverBalanceAfter).to.equal(receiverBalanceBefore + amountsToSend[0]);
+      expect(receiver1BalanceAfter).to.equal(receiver1BalanceBefore + amountsToSend[1]);
+      expect(contractBalance).to.equal(0);
+    });
+  });
+
+  describe("Events", function () {
+    it("Should emit an event on sendFunds success", async function () {
+      const { txLogger } = await ignition.deploy(TransactionLoggerModule);
+      const amountToSend = ethers.parseEther("1");
+
+      const tx = await txLogger.sendFunds(receiver, { value: amountToSend });
+      const receipt = await tx.wait();
+
+      // Get the block and its timestamp
+      const block = await ethers.provider.getBlock(receipt.blockNumber);
+      const timestamp = block?.timestamp;
+
+      await expect(tx)
+        .to.emit(txLogger, "TransactionSent")
+        .withArgs(signers[0], receiver, amountToSend, timestamp);
+    });
+
+    it("Should emit multiple event on sendMultiFunds success", async function () {
+      const { txLogger } = await ignition.deploy(TransactionLoggerModule);
+      const amountsToSend = [ethers.parseEther("0.1"), ethers.parseEther("0.5")];
+      const totalAmount = ethers.parseEther("0.6");
+
+      // Send funds and get the transaction receipt
+      const tx = await txLogger.sendMultiFunds([receiver, receiver1], amountsToSend, {
+        value: totalAmount,
       });
+      const receipt = await tx.wait();
+
+      // Get the block and its timestamp
+      const block = await ethers.provider.getBlock(receipt.blockNumber);
+      const timestamp = block?.timestamp;
+
+      await expect(tx)
+        .to.emit(txLogger, "TransactionSent")
+        .withArgs(signers[0], receiver, amountsToSend[0], timestamp);
+
+      await expect(tx)
+        .to.emit(txLogger, "TransactionSent")
+        .withArgs(signers[0], receiver1, amountsToSend[1], timestamp);
     });
   });
 });
